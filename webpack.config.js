8b8336f0a5939781d3ae517b89d8d45501ff84ed
jinglin <1430622494@qq.com>
2018-10-30 11:29:15 +0800

    creat webpack learn project

---
+++ webpack.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    entry: './src/main.js',
+    output:{
+        // path: '输出文件的绝对路径'
+        // wenpack4.x默认输出路径为dist目录，如果要修改输入路径，就填写path
+        filename:'bundle.js'
+    }
+}
